01d1e63cc29dbadf61da607138ee1b2529478ba4
MewwMinh <maotminh2003@gmail.com>
2024-10-14 19:27:13 +0700

    xoa tam X2

--- Manager/DormitoryManager/DormitoryManager/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DormitoryManager.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
