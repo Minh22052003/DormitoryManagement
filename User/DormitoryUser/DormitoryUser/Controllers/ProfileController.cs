fa1ba3993273f12136d147a50f231eefb19302ef
Minh22052003 <alanpham2224@gmail.com>
2024-10-31 00:23:47 +0700

    Update 0h23 31/10

---
+++ User/DormitoryUser/DormitoryUser/Controllers/ProfileController.cs
@@ -0,0 +1,33 @@
+﻿using DormitoryUser.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DormitoryUser.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            var profile = new Profile
+            {
+                FullName = "Mao Tuan Minh",
+                StudentId = "211211856",
+                CurrentRoom = "IC456879",
+                DateOfBirth = new DateTime(2003, 5, 16),
+                Gender = "Nam", // có thể sử dụng "Nữ" cho nữ
+                PhoneNumber = "0382212381",
+                Email = "student@example.com",
+                Hometown = "Hà Nội", // có thể để trống hoặc thay đổi
+                IdentityCard = "123456789",
+                SocialInsuranceNumber = "987654321",
+                FamilyPhoneNumber = "0987654321",
+                NotificationAddress = "123 Đường ABC, Hà Nội"
+            };
+
+            return View(profile);
+        }
+
+     
+
+    }
+
+}
---
+++ User/DormitoryUser/DormitoryUser/Models/VehicleInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace DormitoryUser.Models
+{
+    public class VehicleInfo
+    {
+        public string VehicleType { get; set; }      // Loại xe (ví dụ: Ô tô, Xe máy)
+        public string LicensePlate { get; set; }     // Biển số xe
+        public string VehicleImageUrl { get; set; }  // URL hình ảnh của xe
+        public List<ParkingHistory> ParkingHistories { get; set; }
+    }
+}
+
