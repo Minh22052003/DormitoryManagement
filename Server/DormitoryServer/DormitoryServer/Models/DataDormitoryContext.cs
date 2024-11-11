324d36d9c363cfe2fe799ebd970169aba8d629e6
MewwMinh <maotminh2003@gmail.com>
2024-11-11 13:32:23 +0700

    Chinh sua

--- Server/DormitoryServer/DormitoryServer/Models/DataDormitoryContext.cs
+++ Server/DormitoryServer/DormitoryServer/Models/DataDormitoryContext.cs
@@ -50,7 +50,7 @@ namespace DormitoryServer.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-CNFI749\\SQLEXPRESS;Initial Catalog=DataDormitory;Integrated Security=True;Multiple Active Result Sets=True;Trust Server Certificate=True");
+                optionsBuilder.UseSqlServer("Data Source=MEWMINH;Initial Catalog=DataDormitory;Integrated Security=True;Multiple Active Result Sets=True;Trust Server Certificate=True");
             }
         }
 
--- Server/DormitoryServer/DormitoryServer/obj/Debug/net6.0/DormitoryServer.AssemblyInfo.cs
+++ Server/DormitoryServer/DormitoryServer/obj/Debug/net6.0/DormitoryServer.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DormitoryServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32ae1046dae6996fe8ed49d56ebe33572f4caf9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+972f98de692c35dd08de1c414497a050e9efa036")]
 [assembly: System.Reflection.AssemblyProductAttribute("DormitoryServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DormitoryServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
